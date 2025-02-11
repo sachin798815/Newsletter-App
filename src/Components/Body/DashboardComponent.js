965be8b2eb901f12c061c14cd4276d09f74b70cf
sachin798815 <sachin798815@gmail.com>
2025-02-11 12:36:18 +0530

    Dashboard template added

---
+++ src/Components/Body/DashboardComponent.js
@@ -0,0 +1,10 @@
+const DashboardComponent = () => {
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+      <p>Manage newsletters and subscribers.</p>
+    </div>
+  );
+};
+
+export default DashboardComponent;
